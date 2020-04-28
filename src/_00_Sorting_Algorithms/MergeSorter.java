4fe6add8814a621bc20906890d65c3f145da08f5
LelsersLasers <48894200+LelsersLasers@users.noreply.github.com>
2020-04-27 19:49:55 -0700

    Finished Sorting, now on to finding

--- src/_00_Sorting_Algorithms/MergeSorter.java
+++ src/_00_Sorting_Algorithms/MergeSorter.java
@@ -11,56 +11,71 @@ public class MergeSorter extends Sorter {
 	//   You can use display.updateDisplay() to show the current
 	//   progress on the graph.
 	@Override
-	void sort(int[] array, SortingVisualizer display) {
+	void sort(int[] arr, SortingVisualizer display) {
 		//20. call the mergeSort method with 0 and the length of the array minus one
+		mergeSort(arr, 0, arr.length - 1, display);
 	}
 	
-	private void mergeSort(int[] array, int low, int high, SortingVisualizer display) {
+	private void mergeSort(int[] arr, int low, int high, SortingVisualizer display) {
+		display.updateDisplay();
 		//1. Create a temporary integer array that is the same length as the passed in array.
-		
+		int[] arr2 = new int[arr.length];
 		//2. make an if statement that checks if low is less than high
 		//   and put the rest of the method inside of it
-
+		if (low < high) {
 			//3. Create an integer called middle and set it 
 			//   equal to the half way point between low and high
-            
+			int middle = (low + high) / 2;
             //4. call the mergeSort method with low and middle
-           
+           mergeSort(arr, low, middle, display);
             //5. call the mergeSort method with middle + 1 and high
-            
+           mergeSort(arr, middle + 1, high, display);
             //6. copy the elements from the array into the temporary array,
             //   but only the elements from low to high inclusive
-            
+            for (int i = low; i <= high; i++) {
+				arr2[i] = arr[i];
+			}
             //7. create three integers called i, j, and k and
             //   set them equal to low, middle + 1, and low respectively
-            
+            int i = low;
+            int j = middle + 1;
+            int k = low;
             //8. while i is less than or equal to middle
             //   and j is less than or equal to high
-            
+            while (i <= middle && j <= high) {
+            	display.updateDisplay();
+            	//display.updateDisplay();
             	//9. if temp array at i is less than or equal 
             	//   to temp array at j	
-                
+                if (arr2[i] <= arr2[j]) {
                     //10. set array at k equal to temp array at i
-                    
+                    arr[k] = arr2[i];
                     //11. increase i by 1
-                  
+                    i = i  + 1;
+                }
                 //13. else
+                else {
             
                     //14. set array at k equal to temp array at j
-                   
+                   arr[k] = arr2[j];
                     //15. increase j by 1
-                 
+                   j = j + 1;
+                }
                 //16. increase k by 1
+                k = k + 1;
                 
-            
+            }
             //17. make a while loop that runs while i is less than or equal to middle
-            
+            while (i <= middle) {
             	//18. set array at k equal to temp array at i
-                
+                arr[k] = arr2[i];
                 //19. increase k and i by 1
+                k = k + 1;
+                i = i + 1;
+            }
                
             
-        
+		}
 	}
 
 }
--- src/_00_Sorting_Algorithms/QuickSorter.java
+++ src/_00_Sorting_Algorithms/QuickSorter.java
@@ -11,8 +11,9 @@ public class QuickSorter extends Sorter {
 	//  You can use display.updateDisplay() to show the current
 	//   progress on the graph.
 	@Override
-	void sort(int[] array, SortingVisualizer display) {
+	void sort(int[] arr, SortingVisualizer display) {
 		//10. call the quickSort method with 0 and the length of the array minus one
+		quickSort(arr, 0, arr.length - 1, display);
 	}
 	
 	private void quickSort(int[] arr, int low, int high, SortingVisualizer display) {
@@ -23,25 +24,43 @@ public class QuickSorter extends Sorter {
        
         //3. create an integer called pivot and set it equal to the element
         //   in the array that is the halfway point between low and high
-		int pivot = arr[(low + high) / 2]; ///I DONT GET THE LANGAUGE, does it want the number inbetween low and high, or the index of the number inbetween low and high
+		int pivot = arr[(low + high) / 2]; 
         //4. make a while loop that goes while i is less than or equal to j
-        
+        while (i <= j) {
+        	display.updateDisplay();
+	
         	//5. make a while loop that increments i while 
         	//   the array element at i is less than pivot
-            
+            while(arr[i] < pivot) {
+            	i = i + 1;
+            }
             //6. make a while loop that decrements j while 
         	//   the array element at j is greater than pivot
-            
+            while(arr[j] > pivot) {
+            	j = j - 1;
+            }
             //7. if i is less than or equal to j, 
             //   swap the array elements at i and j.
             //   In the same if-statement, increase i by 1 
             //   and decrease j by 1.
-        
+            if (i <= j) {
+            	int a = arr[j];
+            	arr[j] = arr[i];
+            	arr[i] = a;
+            	i = i + 1;
+            	j = j - 1;
+            } 
+        }
         //8. if low is less than j, call the quickSort method using
         //   low for the low and j for the high
-        
+        if (low < j) {
+        	quickSort(arr, low, j, display);
+        }
         //9. if i is less than high, call the quickSort method using
         //   i for the low and high for the high.
+        if (i < high) {
+        	quickSort(arr, i, high, display);
+        }
 	}
 
 }
